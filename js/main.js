a0456564bdca072d71f3291b2b45e3184a517794
XakimAA <malutok@mail.ru>
2018-07-27 21:20:18 +0500

    пробная таблица с фильтрацией

--- js/main.js
+++ js/main.js
@@ -36,6 +36,27 @@ function main()
     createTable();
 }
 
+function filterTable($table) {
+    var $filters = $table.find('.table-filters td');
+    var $rows = $table.find('.table-data');
+    $rows.each(function (rowIndex) {
+        var valid = true;
+        $(this).find('td').each(function (colIndex) {
+            if ($filters.eq(colIndex).find('input').val()) {
+                if ($(this).html().toLowerCase().indexOf(
+                $filters.eq(colIndex).find('input').val().toLowerCase()) == -1) {
+                    valid = valid && false;
+                }
+            }
+        });
+        if (valid === true) {
+            $(this).css('display', '');
+        } else {
+            $(this).css('display', 'none');
+        }
+    });
+}
+
   
 function projectTable()
 {
